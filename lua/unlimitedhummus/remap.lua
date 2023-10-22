dac62c72233c82aa66052b6030c7ce36dd3047c5
Magnus Balzer <magnus.balzer@gmail.com>
2023-10-22 22:58:37 +0200

    added remap for inserting and p

--- lua/unlimitedhummus/remap.lua
+++ lua/unlimitedhummus/remap.lua
@@ -1,10 +1,17 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- use system clipboard sometimes
 vim.keymap.set({"n","v"}, "<leader>y", [["+y]])
 vim.keymap.set({"n","v"}, "<leader>Y", [["+Y]])
 vim.keymap.set({"n","v"}, "<leader>d", [["+d]])
 vim.keymap.set({"n","v"}, "<leader>D", [["+D]])
+
+-- better than shift insert?
+vim.keymap.set({"n", "v"}, "<leader>pp", [["+p]])
+
+-- dont replace register
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
