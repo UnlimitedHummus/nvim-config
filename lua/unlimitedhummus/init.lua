f68f1c9f5746ef86b27725d256b231bc30025be2
Magnus Balzer <magnus.balzer@gmail.com>
2023-10-22 22:45:35 +0200

    add telescope plugin and config

--- lua/unlimitedhummus/init.lua
+++ lua/unlimitedhummus/init.lua
@@ -14,6 +14,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({"nvim-treesitter/nvim-treesitter", { "catppuccin/nvim", lazy = false, name = "catppuccin", priority = 1000, config = function()
-vim.cmd([[colorscheme catppuccin]])
-end,}})
+require("lazy").setup({"nvim-treesitter/nvim-treesitter",
+     {
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { "catppuccin/nvim", lazy = false, name = "catppuccin", priority = 1000, config = function()
+    vim.cmd([[colorscheme catppuccin]])
+    end,
+}})
