4af308638d401b73f496b2fbf8e91cc93d6a31d6
Magnus Balzer <magnus.balzer@gmail.com>
2023-10-22 15:59:05 +0200

    Added catppuccin colorscheme

--- lua/unlimitedhummus/init.lua
+++ lua/unlimitedhummus/init.lua
@@ -1,5 +1,6 @@
 require("unlimitedhummus.remap")
 require("unlimitedhummus.set")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -12,4 +13,7 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({"nvim-treesitter/nvim-treesitter"})
+
+require("lazy").setup({"nvim-treesitter/nvim-treesitter", { "catppuccin/nvim", lazy = false, name = "catppuccin", priority = 1000, config = function()
+vim.cmd([[colorscheme catppuccin]])
+end,}})
