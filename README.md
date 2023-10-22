d1cef9d071b9e19da3ba93e5d03c070550f5381c
Magnus Balzer <magnus.balzer@gmail.com>
2023-10-22 14:58:26 +0200

    Added some general remaps

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # My nvim config files
 Plugins are managed using [lazy.nvim](https://github.com/folke/lazy.nvim).
+
+Some remaps require adding system clipboard integration (e.g. having xclip installed)
---
+++ lua/unlimitedhummus/remap.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set({"n","v"}, "<leader>y", [["+y]])
+vim.keymap.set({"n","v"}, "<leader>Y", [["+Y]])
+vim.keymap.set({"n","v"}, "<leader>d", [["+d]])
+vim.keymap.set({"n","v"}, "<leader>D", [["+D]])
+
+vim.keymap.set("n", "<leader><leader>", function()
+	vim.cmd("so")
+end)
